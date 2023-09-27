914237d17c3a3c5d9edb7250da022f2d91cca221
gilberthappi <gdushimimana6@gmail.com>
2023-09-28 00:51:15 +0200

    Adding calculation and operation

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -6,30 +7,41 @@ class Calculator extends React.Component {
     this.state = {};
   }
 
+  clickHandler = (e) => {
+    const result = calculate(this.state, e.target.textContent);
+    this.setState(result);
+  }
+
   render() {
+    const { total, next, operation } = this.state;
+
     return (
       <div className="container">
-        <div className="output">0</div>
+        <div className="output" id="output">
+          {total && total}
+          {operation && operation}
+          {next && next}
+        </div>
         <div className="calculatorBtns">
-          <button className="acBtn gray" type="button">AC</button>
-          <button className="plusMinusBtn gray" type="button">+/-</button>
-          <button className="PercentageBtn gray" type="button">%</button>
-          <button className="operatorBtn" type="button">+</button>
-          <button className="digitBtn" type="button">7</button>
-          <button className="digitBtn" type="button">8</button>
-          <button className="digitBtn" type="button">9</button>
-          <button className="operatorBtn" type="button">x</button>
-          <button className="digitBtn" type="button">4</button>
-          <button className="digitBtn" type="button">5</button>
-          <button className="digitBtn" type="button">6</button>
-          <button className="operatorBtn" type="button">-</button>
-          <button className="digitBtn" type="button">1</button>
-          <button className="digitBtn" type="button">2</button>
-          <button className="digitBtn" type="button">3</button>
-          <button className="operatorBtn" type="button">+</button>
-          <button className="digitBtn span2" type="button">0</button>
-          <button className="digitBtn" type="button">.</button>
-          <button className="evaluationBtn" type="button">=</button>
+          <button className="acBtn gray" type="button" onClick={this.clickHandler}>AC</button>
+          <button className="plusMinusBtn gray" type="button" onClick={this.clickHandler}>+/-</button>
+          <button className="PercentageBtn gray" type="button" onClick={this.clickHandler}>%</button>
+          <button className="operatorBtn" type="button" onClick={this.clickHandler}>÷</button>
+          <button className="digitBtn" type="button" onClick={this.clickHandler}>7</button>
+          <button className="digitBtn" type="button" onClick={this.clickHandler}>8</button>
+          <button className="digitBtn" type="button" onClick={this.clickHandler}>9</button>
+          <button className="operatorBtn" type="button" onClick={this.clickHandler}>x</button>
+          <button className="digitBtn" type="button" onClick={this.clickHandler}>4</button>
+          <button className="digitBtn" type="button" onClick={this.clickHandler}>5</button>
+          <button className="digitBtn" type="button" onClick={this.clickHandler}>6</button>
+          <button className="operatorBtn" type="button" onClick={this.clickHandler}>-</button>
+          <button className="digitBtn" type="button" onClick={this.clickHandler}>1</button>
+          <button className="digitBtn" type="button" onClick={this.clickHandler}>2</button>
+          <button className="digitBtn" type="button" onClick={this.clickHandler}>3</button>
+          <button className="operatorBtn" type="button" onClick={this.clickHandler}>+</button>
+          <button className="digitBtn span2" type="button" onClick={this.clickHandler}>0</button>
+          <button className="digitBtn" type="button" onClick={this.clickHandler}>.</button>
+          <button className="evaluationBtn" type="button" onClick={this.clickHandler}>=</button>
         </div>
       </div>
     );
