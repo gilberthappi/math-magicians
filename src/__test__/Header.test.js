85b410eeba232f6f78e2bb8841a306cc5a86417c
Ivonne <ivonnebenites2015@gmail.com>
2023-10-05 14:52:42 -0500

    Update and add test files

--- src/__test__/Header.test.js
+++ src/__test__/Header.test.js
@@ -1,13 +1,12 @@
-// // eslint-disable-next-line import/no-extraneous-dependencies
-// import renderer from 'react-test-renderer';
-// import { BrowserRouter } from 'react-router-dom';
-// import Header from '../components/Header';
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import Header from '../components/Header';
 
-// describe('Header component testing', () => {
-//   it('Renders correctly', () => {
-//     const tree = renderer
-//       .create(<BrowserRouter><Header /></BrowserRouter>)
-//       .toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
-// });
+describe('Header component testing', () => {
+  it('Renders correctly', () => {
+    const tree = renderer
+      .create(<BrowserRouter><Header /></BrowserRouter>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/__test__/calculate.test.js
+++ src/__test__/calculate.test.js
@@ -1,69 +1,69 @@
-// import calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 
-// let obj = { total: null, next: null, operation: null };
+let obj = { total: null, next: null, operation: null };
 
-// describe('Testing calculate file', () => {
-//   test('Reset', () => {
-//     obj = calculate(obj, '13');
-//     obj = calculate(obj, '+');
-//     obj = calculate(obj, '5');
-//     obj = calculate(obj, 'AC');
-//     expect(obj.total).toBe(null);
-//   });
+describe('Testing calculate file', () => {
+  test('Reset', () => {
+    obj = calculate(obj, '13');
+    obj = calculate(obj, '+');
+    obj = calculate(obj, '5');
+    obj = calculate(obj, 'AC');
+    expect(obj.total).toBe(null);
+  });
 
-//   test('Summation', () => {
-//     obj = calculate(obj, '13');
-//     obj = calculate(obj, '+');
-//     obj = calculate(obj, '5');
-//     obj = calculate(obj, '=');
-//     expect(obj.total).toBe('18');
-//   });
+  test('Summation', () => {
+    obj = calculate(obj, '13');
+    obj = calculate(obj, '+');
+    obj = calculate(obj, '5');
+    obj = calculate(obj, '=');
+    expect(obj.total).toBe('18');
+  });
 
-//   test('Subtraction', () => {
-//     obj = calculate(obj, '13');
-//     obj = calculate(obj, '-');
-//     obj = calculate(obj, '5');
-//     obj = calculate(obj, '=');
-//     expect(obj.total).toBe('8');
-//   });
+  test('Subtraction', () => {
+    obj = calculate(obj, '13');
+    obj = calculate(obj, '-');
+    obj = calculate(obj, '5');
+    obj = calculate(obj, '=');
+    expect(obj.total).toBe('8');
+  });
 
-//   test('Multiplication', () => {
-//     obj = calculate(obj, '13');
-//     obj = calculate(obj, 'x');
-//     obj = calculate(obj, '5');
-//     obj = calculate(obj, '=');
-//     expect(obj.total).toBe('65');
-//   });
+  test('Multiplication', () => {
+    obj = calculate(obj, '13');
+    obj = calculate(obj, 'x');
+    obj = calculate(obj, '5');
+    obj = calculate(obj, '=');
+    expect(obj.total).toBe('65');
+  });
 
-//   test('Division', () => {
-//     obj = calculate(obj, '40');
-//     obj = calculate(obj, '÷');
-//     obj = calculate(obj, '5');
-//     obj = calculate(obj, '=');
-//     expect(obj.total).toBe('8');
-//   });
+  test('Division', () => {
+    obj = calculate(obj, '40');
+    obj = calculate(obj, '÷');
+    obj = calculate(obj, '5');
+    obj = calculate(obj, '=');
+    expect(obj.total).toBe('8');
+  });
 
-//   test('Division By Zero', () => {
-//     obj = calculate(obj, '13');
-//     obj = calculate(obj, '÷');
-//     obj = calculate(obj, '0');
-//     obj = calculate(obj, '=');
-//     expect(obj.total).toBe("Can't divide by 0.");
-//   });
+  test('Division By Zero', () => {
+    obj = calculate(obj, '13');
+    obj = calculate(obj, '÷');
+    obj = calculate(obj, '0');
+    obj = calculate(obj, '=');
+    expect(obj.total).toBe("Can't divide by 0.");
+  });
 
-//   test('Modulo operation', () => {
-//     obj = calculate(obj, '13');
-//     obj = calculate(obj, '%');
-//     obj = calculate(obj, '5');
-//     obj = calculate(obj, '=');
-//     expect(obj.total).toBe('3');
-//   });
+  test('Modulo operation', () => {
+    obj = calculate(obj, '13');
+    obj = calculate(obj, '%');
+    obj = calculate(obj, '5');
+    obj = calculate(obj, '=');
+    expect(obj.total).toBe('3');
+  });
 
-//   test('Modulo operation By Zero', () => {
-//     obj = calculate(obj, '13');
-//     obj = calculate(obj, '%');
-//     obj = calculate(obj, '0');
-//     obj = calculate(obj, '=');
-//     expect(obj.total).toBe("Can't find modulo as can't divide by 0.");
-//   });
-// });
+  test('Modulo operation By Zero', () => {
+    obj = calculate(obj, '13');
+    obj = calculate(obj, '%');
+    obj = calculate(obj, '0');
+    obj = calculate(obj, '=');
+    expect(obj.total).toBe("Can't find modulo as can't divide by 0.");
+  });
+});
